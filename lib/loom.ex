eb2fc237b618f763b8eaa8744a41300ef8c26d32
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-08-11 14:46:40 -0600

    Add a lil doc blurb

--- lib/loom.ex
+++ lib/loom.ex
@@ -1,5 +1,12 @@
 defmodule Loom do
   @moduledoc """
-  Documentation for `Loom`.
+  Loom is a filesystem-based event store.
+
+  All Loom functions require a base directory argument.
+  Inside that directory, events are created at `events/<event-id>.json`,
+  and streams are created at `streams/<stream-id>/<revison-number>.json`.
+  The files in each stream directory are symlinks to events in `events/`.
+
+  Events are JSON-encoded according to the [CloudEvents spec](cloudevents.io).
   """
 end
