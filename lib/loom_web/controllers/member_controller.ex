8d85ff7eb4a6a78626980f9a68a7f7619886c3ae
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-12-13 08:31:52 -0700

    Create a team member index

--- lib/loom_web/controllers/member_controller.ex
+++ lib/loom_web/controllers/member_controller.ex
@@ -1,3 +1,11 @@
 defmodule LoomWeb.MemberController do
   use LoomWeb, :controller
+
+  alias Loom.Accounts
+
+  def index(conn, %{"team_id" => team_id}) do
+    team = Accounts.get_team!(team_id)
+
+    render(conn, :index, team: team, users: team.users)
+  end
 end
---
+++ lib/loom_web/views/member_view.ex
@@ -0,0 +1,13 @@
+defmodule LoomWeb.MemberView do
+  use LoomWeb, :view
+
+  def user_gravatar_url(user) do
+    email =
+      String.trim(user.email)
+      |> String.downcase()
+
+    hash = :crypto.hash(:md5, email) |> Base.encode16(case: :lower)
+
+    "https://gravatar.com/avatar/#{hash}?d=identicon"
+  end
+end
