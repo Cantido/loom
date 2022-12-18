f22ab90eaa56594ba217a5c74ba20beb2eb4d3a6
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-12-17 20:30:14 -0700

    Clean up auth

--- lib/loom_web/token_bearer_auth_pipeline.ex
+++ lib/loom_web/token_bearer_auth_pipeline.ex
@@ -2,8 +2,7 @@ defmodule LoomWeb.TokenBearerAuthPipeline do
   use Guardian.Plug.Pipeline,
     otp_app: :loom,
     module: LoomWeb.Tokens,
-    error_handler: LoomWeb.AuthErrorHandler,
-    key: :current_token
+    error_handler: LoomWeb.AuthErrorHandler
 
   plug Guardian.Plug.VerifyHeader
   plug Guardian.Plug.EnsureAuthenticated
--- lib/loom_web/tokens.ex
+++ lib/loom_web/tokens.ex
@@ -1,71 +1,19 @@
 defmodule LoomWeb.Tokens do
   @moduledoc """
   A `Guardian` module for generating authentication tokens.
-
-  Loom uses two different subjects for auth tokens:
-
-  - `Loom.Accounts.User` structs, for browser authentication
-  - `Loom.Accounts.Token` structs, for API authentication
-
-  Subjects are formatted as strings starting with either `"user:"` or `"token:"` followed by the struct's Ecto UUID, formatted as a 22-character, base64-encoded slug.
-
-  ## Examples
-
-      iex> LoomWeb.Tokens.subject_for_token(%Loom.Accounts.User{id: "6ba7b810-9dad-11d1-80b4-00c04fd430c8"})
-      {:ok, "user:a6e4EJ2tEdGAtADAT9QwyA"}
   """
   use Guardian, otp_app: :loom
 
   alias Loom.Accounts
   alias Loom.Accounts.Token
-  alias Loom.Accounts.User
-
-  require Logger
 
   def subject_for_token(%Token{id: id}, _claims) do
-    case Uniq.UUID.parse(id) do
-      {:ok, uuid} ->
-        slug = Uniq.UUID.to_string(uuid, :slug)
-
-        {:ok, "token:" <> slug}
-
-      err ->
-        err
-    end
+    {:ok, id}
   end
 
-  def subject_for_token(%User{id: id}, _claims) do
-    case Uniq.UUID.parse(id) do
-      {:ok, uuid} ->
-        slug = Uniq.UUID.to_string(uuid, :slug)
-
-        {:ok, "user:" <> slug}
-
-      err ->
-        err
-    end
-  end
-
-  def resource_from_claims(%{"sub" => "token:" <> slug}) do
-    case Uniq.UUID.parse(slug) do
-      {:ok, id} ->
-        token = Accounts.get_token!(Uniq.UUID.to_string(id, :default))
-        {:ok, token}
-
-      err ->
-        err
-    end
-  end
-
-  def resource_from_claims(%{"sub" => "user:" <> slug}) do
-    case Uniq.UUID.parse(slug) do
-      {:ok, id} ->
-        user = Accounts.get_user!(Uniq.UUID.to_string(id, :default))
-        {:ok, user}
-
-      err ->
-        err
-    end
+  def resource_from_claims(%{"sub" => id}) do
+    token = Accounts.get_token!(id)
+    {:ok, token}
   end
 
   def build_claims(claims, %Token{} = token, _opts) do
