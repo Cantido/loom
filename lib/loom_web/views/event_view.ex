b8e428714cae9d1a752c879e3e9aed8318670dc3
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-12-22 12:22:30 -0700

    Fix: Event graph handle no data

--- lib/loom_web/views/event_view.ex
+++ lib/loom_web/views/event_view.ex
@@ -45,22 +45,29 @@ defmodule LoomWeb.EventView do
     end_time = DateTime.utc_now() |> DateTime.truncate(:second)
     start_time = DateTime.add(end_time, -2, :minute)
 
-    svg =
-      Enum.map(assigns.events, fn e ->
-        naive_time = DateTime.to_naive(e.time)
-        time_group = NaiveDateTime.truncate(naive_time, :second)
+    events_per_second =
+      if Enum.empty?(assigns.events) do
+        empty_data(start_time, end_time)
+      else
+        Enum.map(assigns.events, fn e ->
+          naive_time = DateTime.to_naive(e.time)
+          time_group = NaiveDateTime.truncate(naive_time, :second)
 
-        e
-        |> Map.from_struct()
-        |> Map.take([:id, :time])
-        |> Map.put(:time, naive_time)
-        |> Map.put(:time_group, time_group)
-      end)
-      |> DataFrame.new()
-      |> DataFrame.arrange(time)
-      |> DataFrame.group_by(:time_group)
-      |> DataFrame.summarise(count: count(id))
-      |> fill_missing(start_time, end_time)
+          e
+          |> Map.from_struct()
+          |> Map.take([:id, :time])
+          |> Map.put(:time, naive_time)
+          |> Map.put(:time_group, time_group)
+        end)
+        |> DataFrame.new()
+        |> DataFrame.group_by(:time_group)
+        |> DataFrame.summarise(count: count(id))
+        |> fill_missing(start_time, end_time)
+        |> DataFrame.arrange(time_group)
+        end
+
+    svg =
+      events_per_second
       |> DataFrame.to_rows()
       |> Contex.Dataset.new()
       |> Contex.Plot.new(Contex.LinePlot, 800, 400, mapping: %{x_col: "time_group", y_cols: ["count"]}, smoothed: false)
@@ -71,18 +78,31 @@ defmodule LoomWeb.EventView do
     ~H"""
     <%= @svg %>
     """
-
   end
 
   defp fill_missing(df, start_datetime, stop_datetime) do
-    timestamps =
-      Stream.iterate(start_datetime, &DateTime.add(&1, 1, :second))
-      |> Stream.take_while(&DateTime.compare(&1, stop_datetime) in [:lt, :eq])
-      |> Stream.map(&DateTime.to_naive/1)
-      |> Enum.to_list()
+    timestamps = datetime_range(start_datetime, stop_datetime)
 
     DataFrame.new(time_group: timestamps)
     |> DataFrame.join(df, how: :left)
     |> DataFrame.mutate_with(&[count: Explorer.Series.fill_missing(&1["count"], 0)])
+  end
+
+  defp empty_data(start_datetime, stop_datetime) do
+    datetime_range(start_datetime, stop_datetime)
+    |> Enum.map(fn datetime ->
+      %{
+        time_group: datetime,
+        count: 0
+      }
+    end)
+    |> DataFrame.new()
+  end
+
+  defp datetime_range(start_datetime, stop_datetime) do
+    Stream.iterate(start_datetime, &DateTime.add(&1, 1, :second))
+    |> Stream.take_while(&DateTime.compare(&1, stop_datetime) in [:lt, :eq])
+    |> Stream.map(&DateTime.to_naive/1)
+    |> Enum.to_list()
   end
 end
